b688b0cf12035788f5d78a02bc189db77a849cbe
Jason z <35103207+xuan32546@users.noreply.github.com>
2021-02-24 14:01:59 +0800

    git-update

---
+++ layout/usr/lib/python3.7/site-packages/zxtouch/backup/deviceinfotasktypes.py
@@ -0,0 +1,9 @@
+DEVICE_INFO_TASK_GET_SCREEN_SIZE = 1
+DEVICE_INFO_TASK_GET_SCREEN_ORIENTATION = 2
+DEVICE_INFO_TASK_GET_SCREEN_SCALE = 3
+
+# 1-30 reserved for screen
+
+
+DEVICE_INFO_TASK_GET_DEVICE_INFO = 30 # including device name, opearting system name, model name, system version
+DEVICE_INFO_TASK_GET_BATTERY_INFO = 31
